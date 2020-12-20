c4eec620c53abacc1ecfa980a8cf33360352ffc6
burnt776 <burnt776@gmail.com>
2020-12-20 17:45:05 +0530

    pop and append

---
+++ stack/stack 1.py
@@ -0,0 +1,11 @@
+stack =[]
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print('inital stack')
+print(stack)
+
+print('empty stack')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
