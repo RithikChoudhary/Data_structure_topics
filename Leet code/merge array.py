92c2c9b96824077a615606888f0e149c0407aa4b
burnt776 <burnt776@gmail.com>
2020-12-25 16:33:18 +0530

    done with medain program

---
+++ Leet code/merge array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1=sorted(nums1)
+        # print(nums1)
+        l=len(nums1)
+        if l%2==0:
+            return ((nums1[(l//2)]+nums1[((l//2)-1)])/2)
+        else:
+            return nums1[l//2]
---
+++ Python/Tuples/tuple1.py
@@ -0,0 +1,13 @@
+# create tuple
+# tup = (1,2,3,4,12,4,3,412)
+# diff_data_type = (1.2,"rithik",32.4,"ram","rithik")
+# print(diff_data_type)
+
+
+tup = (1,2,3,4,5)
+# print(tup[2])
+
+# n,m,b,v,c = tup
+# print(n+m+b+v+c)
+tup = tup + (12)
+print(tup)
