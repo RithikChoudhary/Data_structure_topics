1314bb119c7ca487c66bb784a1602c38ae2584fe
burnt776 <burnt776@gmail.com>
2021-01-02 10:01:51 +0530

    Arrary 1

--- questions/1_jan/Array1.py
+++ questions/1_jan/Array1.py
@@ -12,9 +12,30 @@
 # Input  : arr[] = {1, 2, 3}
 # Output : arr[] = {3, 2, 1}
 
+
+# def reverseList(A):
+#   print( A[::-1])
+     
+# # Driver function to test above function 
+# A = list(map(int,input().strip().split()))
+# print(A) 
+# print("Reversed list is") 
+# reverseList(A)  
+
+    
+def reverseList(A,end,start):
+    i = end
+    reverseFinal = []
+    while i >=start:
+        reverseFinal.append(A[i])
+        i-=1
+    print(reverseFinal)
+    
 if __name__ == '__main__':
+    A = [1,2,3,4,5,6,7,8]
+    end = len(A)-1
+    start = 0
+     
+    reverseList(A,end,start)
     
-    revers =  list(map(int,input().strip().split()))
-    
-   
     
