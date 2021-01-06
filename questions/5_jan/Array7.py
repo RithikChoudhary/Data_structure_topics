cece4bef0472e706564f553f353051b15b0c5719
burnt776 <burnt776@gmail.com>
2021-01-06 14:38:27 +0530

    solved

--- questions/5_jan/Array7.py
+++ questions/5_jan/Array7.py
@@ -1,8 +1,14 @@
 def rotate(list1):
-    zoom=[]
-    for i in list1[-1:-5:-4]:
-        print(i)
-    return (zoom)    
+    num = list1[len(list1)-1]
+    
+    for i in range(len(list1)-1,-1,-1):
+        list1[i]= list1[i-1]
+    
+    list1[0] = num
+    return list1
+   
+   
+        
 if __name__ == '__main__':
     test = int(input())
     see = []
