f947a6ba8674e1dfec52e3ac5a85a26681efd43b
burnt776 <burnt776@gmail.com>
2021-01-13 12:17:51 +0530

    done with todo list

---
+++ questions/12_jan/Array12.py
@@ -0,0 +1,13 @@
+def merge(arr1,arr2):
+    arr = []
+    for i,j in arr1,arr2:
+         
+
+
+if __name__ == '__main__':
+    n,m = map(int,input().strip().split())
+    arr1 = list(map(int,input().strip().split()))
+    arr2 = list(map(int,input().strip().split()))
+    merge(arr1,arr2)
+    
+    
--- test.py
+++ test.py
@@ -1,14 +1,140 @@
-arr = [1,0,4,6,0,2,3]
-arr1=[]
-count=0
-for i in range(len(arr)):
-    if arr[i] == 0:
-        count+=1
-    else:
-        arr1.append(arr[i])
-for j in range(count):
-    arr1.append(0)
-print(arr1)
+# Output List, The Main List that we are performing our ops
+outputList = []
 
+# The STACK which keeps the history of commands priviously used
+undoTask = []
 
+# The STACK which keeps the history of commands priviously undone
+redoTask = []
 
+def add(arg):
+	# add function to literally add elements to the outputList
+	global outputList, undoTask
+	undoTask.append(('append', arg, len(outputList)))
+	outputList.append(arg)
+
+def remove(arg):
+	# remove function to literally remove elements to the outputList
+	global outputList, undoTask
+	undoTask.append(('remove', arg, outputList.index(arg)))
+	outputList.remove(arg)
+
+def undo():
+	# remove function to literally remove elements to the outputList
+	global outputList, undoTask, redoTask
+	task, val, index = undoTask.pop()
+	if task == 'remove':
+		outputList.insert(index, val)
+		redoTask.append( ('append', val, index) )
+
+	elif task == 'append':
+		outputList.pop(index)
+		redoTask.append( ('remove', val, index) )
+
+def redo():
+	global outputList, undoTask, redoTask
+	task, val, index = redoTask.pop()
+
+	if task == 'remove':
+		outputList.insert(index, val)
+
+	elif task == 'append':
+		outputList.pop(index)
+	
+	undoTask.append( (task, val, index) )
+
+if __name__ == '__main__':
+	add(1)
+	print("add(1) -> " ,outputList)
+	# print(outputList)
+
+	
+
+	
+	add(2)
+	print("add(2) -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	
+	add(3)
+	print("add(3) -> ",outputList)
+	# print(outputList
+	
+		
+	add(4)
+	print("add(4) -> ",outputList)
+	# print(outputList)
+
+	
+		
+	undo()
+	print("undo() -> ",outputList)
+	# print(outputList)
+
+		
+	undo()
+	print("undo() -> ",outputList )
+	# print(outputList)
+	
+	
+		
+	redo()
+	print("redo() -> ",outputList)
+	# print(outputList)
+
+	
+	
+	redo()
+	print("redo() -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	remove(3)
+	print("remove(3) -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	
+	remove(1)
+	print("remove(1) -> ",outputList)
+	# print(outputList)
+
+	
+	
+	
+	remove(2)
+	print("remove(2) -> ",outputList)
+	# print(outputList)
+
+	
+	
+	
+	undo()
+	print("undo() -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	
+	undo()
+	print("undo() -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	
+	undo()
+	print("undo() -> ",outputList)
+	# print(outputList)
+	
+	
+	
+	
+	redo()
+	print("redo() -> ",outputList)
+	# print(outputList)
+	
